64befadc29d2951ce9bf1217602f2c4d69356972
Arthur Lebron <78775633+SongXiaoYi@users.noreply.github.com>
2025-09-14 20:02:05 +0800

    Update S02.Generate_GMT.R

--- Data_Process/S02.Generate_GMT.R
+++ Data_Process/S02.Generate_GMT.R
@@ -17,7 +17,7 @@ setwd('H:\\Reng')
 gset <- fread('./GeneSet.csv') %>% as.data.frame()
 gset <- split(gset, gset$`Metabolic pathways involved`)
 for (n in 1:length(gset)) {
-  gset[[n]] <- as.list(gset[[n]]$`Gene symbol`)
+  gset[[n]] <- as.list(unique(gset[[n]]$`Gene symbol`))
 }
 
 file=("./gset_total.gmt")
@@ -37,7 +37,14 @@ sink() #结束重定向
 output_gmt(gset,file)
 #######################################################
 gmt <- getGmt('gset_total.gmt')
+pbmc <- pbmc[,-1]
+TCGA <- as.matrix(pbmc)
+TCGA <- avereps(TCGA)
+gs.exp <- gsva(TCGA,gmt,method = "ssgsea",kcdf = "Poisson", min.sz = 1)
 
-
+#DR_score <- gs.exp[190,] - gs.exp[191,]
+setwd('H:\\Reng')
+gs.exp <- as.data.frame(gs.exp)
+write.csv(gs.exp, file = 'Gset_analysis_score.csv', row.names = FALSE)
 
 
