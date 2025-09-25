8d0abca406812d162abf9382e0aec186c8710ce8
Arthur Lebron <78775633+SongXiaoYi@users.noreply.github.com>
2025-09-26 00:00:54 +0800

    Update S05.Pathway_enrichment_heatmap.R

--- Data_Process/S05.Pathway_enrichment_heatmap.R
+++ Data_Process/S05.Pathway_enrichment_heatmap.R
@@ -13,6 +13,7 @@ library(viridis)
 library(Seurat)
 library(ComplexHeatmap)
 library(circlize)
+library(scales)
 ######################
 setwd('H:\\Reng')
 Score_matrix <- read.csv('./Gset_analysis_score.csv')
@@ -38,15 +39,21 @@ Order_cluster <- Cluster_num[order(Cluster_num$clust),'clust']
 Order_cluster <- as.character(Order_cluster)
 Col_split <- Order_cluster
 
-expre <- scale(expre,center = TRUE)
+Plot_data <- expre
+Rescale_column <- function(x) {rescale(x,to = c(-1,1))}
+Plot_data <- apply(Plot_data,1,Rescale_column)
+#expre <- scale(expre,center = TRUE)
 
-Color1 = colorRamp2(seq(-3, 3, length = 10), colorRampPalette(c("blue","white","red"))(10))
+Color1 = colorRamp2(seq(-1, 1, length = 5), colorRampPalette(c("blue","white","red"))(5))
 
-p3 <- Heatmap(expre, name = "Expression", col = Color1, row_split = Row_split, column_names_rot = 45,
+p3 <- Heatmap(t(Plot_data), name = "Enrichment", col = Color1, row_split = Row_split, column_names_rot = 45,
               column_split = Col_split, cluster_rows = TRUE,cluster_columns = TRUE, show_column_names = TRUE,show_row_names = TRUE, #rect_gp = gpar(col = "black", lwd = 2),
               cluster_column_slices = FALSE,row_title_rot = 0)
 
-
+setwd('H:\\Reng')
+pdf(file = 'Pathway_analysis.pdf', width = 8.9, height = 6.65)
+p3
+dev.off()
 
 
 
